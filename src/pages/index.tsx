1129a1016d6a04752e22e34bfef98df8d34807bd
hieuvecto <hieuvecto@gmail.com>
2024-07-02 03:52:21 +0700

    Remove redundants.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,6 @@
 import { trpc } from '../utils/trpc.utils';
 import type { NextPageWithLayout } from './_app';
-import type { inferProcedureInput } from '@trpc/server';
-import Link from 'next/link';
-import { Fragment } from 'react';
-import RestaurantSummary from '~/components/organisms/RestaurantSummary';
 import HomePage from '~/components/templates/Home';
-import type { AppRouter } from '~/server/routers/_app';
-import type { Prisma } from '@prisma/client';
-import type { RestaurantPayload } from '~/types/restaurant.types';
 
 const IndexPage: NextPageWithLayout = () => {
   const restaurantsQuery = trpc.restaurant.list.useQuery({
@@ -19,29 +12,3 @@ const IndexPage: NextPageWithLayout = () => {
 };
 
 export default IndexPage;
-
-/**
- * If you want to statically render this page
- * - Export `appRouter` & `createContext` from [trpc].ts
- * - Make the `opts` object optional on `createContext()`
- *
- * @link https://trpc.io/docs/v11/ssg
- */
-// export const getStaticProps = async (
-//   context: GetStaticPropsContext<{ filter: string }>,
-// ) => {
-//   const ssg = createServerSideHelpers({
-//     router: appRouter,
-//     ctx: await createContext(),
-//   });
-//
-//   await ssg.post.all.fetch();
-//
-//   return {
-//     props: {
-//       trpcState: ssg.dehydrate(),
-//       filter: context.params?.filter ?? 'all',
-//     },
-//     revalidate: 1,
-//   };
-// };
